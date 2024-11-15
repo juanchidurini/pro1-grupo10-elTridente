9531891a46c05aa0e449af94559dd6b0730e0f61
bauticenci <bauti7c@gmail.com>
2024-11-15 12:57:01 -0300

    hicimos todo el java del home,buscador

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
-console.log('hola');
+let buscador = document.querySelector(".buscador")
+let sectionRecetas = document.querySelector(".sectionRecetas")
+let url = 'https://dummyjson.com/recipes?limit=10'
+let formulario = document.querySelector(".formulario")
+let botonCargarMas = document.querySelector(".BotonCargarMas")
+
+
+function obtenerRecetas(url) {
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let dato = data.recipes;
+            let recetas = "";
+            for (let i = 0; i < dato.length; i++) {
+                recetas += `<article>
+                                <img src="${dato[i].image}" alt="">
+                                <p> "${dato[i].name}" </p>
+                                <p> "${dato[i].difficulty}"</p>
+                                <a href="./detalleReceta.html"> ir a detalle </a> 
+                    </article>`
+            }
+            sectionRecetas.innerHTML = recetas
+        })
+        .catch(function (error) {
+            console.log("error: " + error);
+
+        })
+}
+
+formulario.addEventListener("submit", function (e) {
+    e.preventDefault()
+    if (buscador.value == "") {
+        alert("Tiene que escribir algo")
+    } else if (buscador.value.length < 3) {
+        alert("Tiene que tener mas de 3 caracteres")
+    } else {
+        this.submit()
+    }
+
+})
+
+obtenerRecetas(url)
+
+botonCargarMas.addEventListener("click", function(){
+    obtenerRecetas(url)
+})
+
+
+
+
---
+++ js/resultadoBusqueda.js
@@ -0,0 +1,45 @@
+let qs = location.search;
+let qsObj = new URLSearchParams(qs);
+let buscador = qsObj.get("buscador");
+let sectionRecetas = document.querySelector(".articleRecetas")
+
+
+let url = `https://dummyjson.com/recipes/search?q=${buscador}`;
+function obtenerRecetas(url) {
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let dato = data.recipes;
+
+            if (dato.length==0) {
+                alert("no se a encontrado ninguna receta")
+
+            } else {
+                
+            }
+            console.log(dato);
+
+            let recetas = "";
+            for (let i = 0; i < dato.length; i++) {
+                recetas += `<article>
+                                <img src="${dato[i].image}" alt="">
+                                <p> "${dato[i].name}" </p>
+                                <a href="./detalleReceta.html"> ir a detalle </a> 
+                    </article>`
+            }
+            sectionRecetas.innerHTML = recetas
+
+            
+           
+        })
+        .catch(function (error) {
+            console.log("error: " + error);
+            
+         })
+}
+
+
+
+obtenerRecetas(url);
