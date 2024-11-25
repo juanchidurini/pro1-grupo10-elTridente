42fb012de04aeab9649d00b911a20b7047e0b816
lucas Reynoso <lreynoso@udesa.edu.ar>
2024-11-25 11:45:07 -0300

    cambie la tipografia, avance con el register validando el checkbox

--- js/resgister.js
+++ js/resgister.js
@@ -1,29 +1,31 @@
 let email = document.getElementById("email")
 let contraseña = document.getElementById("contraseña")
 let formularioRegister = document.querySelector(".formularioRegister")
-let terminosYcondiciones = document.getElementById("terminosYcondicones")
+let terminosYcondiciones = document.getElementById("terminosYcondiciones")
 
 
 formularioRegister.addEventListener(`submit`,function(e){
     e.preventDefault();
+    console.log(terminosYcondiciones)
     if (email.value == "") {
         email.placeholder = "complete su email"
        return;
 
     } else if (contraseña.value == "") {
-contraseña.placeholder = "ingrese una contraseña"
+            contraseña.placeholder = "ingrese una contraseña"
         return;
 
     } else if (contraseña.value.length < 3) {
         contraseña.placeholder = "la contraseña debe tener al menos 3 caracteres"
         return;
 
-    } else if (terminosYcondiciones == false){
+    }else if (!terminosYcondiciones.checked){
         return;
-    } else {formularioRegister.submit();}
 
+} else {formularioRegister.submit();}
+    
 
-})
+});
 
 
 
