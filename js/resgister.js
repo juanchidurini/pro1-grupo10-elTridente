225a737d2d63cb72daf6ac6f92b13d5bf18da735
bauticenci <bauti7c@gmail.com>
2024-11-26 11:52:55 -0300

    Modifique el css de detalle receta y un poco el java y css de register para que aparezca en rojo cuando no completas algún campo etc

--- js/resgister.js
+++ js/resgister.js
@@ -2,17 +2,19 @@ let email = document.getElementById("email")
 let contraseña = document.getElementById("contraseña")
 let formularioRegister = document.querySelector(".formularioRegister")
 let terminosYcondiciones = document.getElementById("terminosYcondiciones")
-
+let avisoTerminosYcondiciones = document.querySelector(".avisoTerminosYcondiciones")
 
 formularioRegister.addEventListener(`submit`,function(e){
     e.preventDefault();
     console.log(terminosYcondiciones)
     if (email.value == "") {
         email.placeholder = "complete su email"
+        email.classList.add("input-error");
        return;
 
     } else if (contraseña.value == "") {
             contraseña.placeholder = "ingrese una contraseña"
+            contraseña.classList.add("input-error"); /// esto llama a una clase en css para que modifique el color del placeholfer si es que el campo contraseña esta vacio///
         return;
 
     } else if (contraseña.value.length < 3) {
@@ -20,6 +22,7 @@ formularioRegister.addEventListener(`submit`,function(e){
         return;
 
     }else if (!terminosYcondiciones.checked){
+        avisoTerminosYcondiciones.innerText = "Por favor acepte los terminos y condiciones"
         return;
 
 } else {formularioRegister.submit();}
